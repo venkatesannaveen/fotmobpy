ca763d72758a83fbf1046fd7da26c132690fb073
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2022-02-20 00:11:31 -0800

    Added home and away scores to matches

--- fotmobpy/matches.py
+++ fotmobpy/matches.py
@@ -9,6 +9,8 @@ def get_all_matches(date: str) -> pd.DataFrame:
 
 	for league in r["leagues"]:
 		for match in league["matches"]:
+			home_score = match["home"]["score"] if match["status"]["finished"] else None
+			away_score = match["away"]["score"] if match["status"]["finished"] else None
 			matches.append(
 				{
 					"id": match["id"], 
@@ -16,8 +18,10 @@ def get_all_matches(date: str) -> pd.DataFrame:
 					"league": league["name"],
 					"home_id": match["home"]["id"],
 					"home_name": match["home"]["longName"],
+					"home_score": home_score,
 					"away_id": match["away"]["id"],
-					"away_name": match["away"]["longName"]
+					"away_name": match["away"]["longName"],
+					"away_score": away_score
 				}
 			)
 	
@@ -32,6 +36,8 @@ def get_team_matches(date: str, team: str) -> pd.DataFrame:
 	for league in r["leagues"]:
 		for match in league["matches"]:
 			if match["home"]["longName"].lower() == team or match["away"]["longName"].lower() == team:
+				home_score = match["home"]["score"] if match["status"]["finished"] else None
+				away_score = match["away"]["score"] if match["status"]["finished"] else None
 				matches.append(
 					{
 						"id": match["id"], 
@@ -39,8 +45,10 @@ def get_team_matches(date: str, team: str) -> pd.DataFrame:
 						"league": league["name"],
 						"home_id": match["home"]["id"],
 						"home_name": match["home"]["longName"],
+						"home_score": home_score,
 						"away_id": match["away"]["id"],
-						"away_name": match["away"]["longName"]
+						"away_name": match["away"]["longName"],
+						"away_score": away_score
 					}
 				)
 	
@@ -57,6 +65,8 @@ def get_league_matches(date: str, country: str, league_name: str=None) -> pd.Dat
 			for match in league["matches"]:
 				if league_name is not None and league["name"].lower() != league_name.lower():
 					continue
+				home_score = match["home"]["score"] if match["status"]["finished"] else None
+				away_score = match["away"]["score"] if match["status"]["finished"] else None
 				matches.append(
 					{
 						"id": match["id"],
@@ -64,8 +74,10 @@ def get_league_matches(date: str, country: str, league_name: str=None) -> pd.Dat
 						"league": league["name"],
 						"home_id": match["home"]["id"],
 						"home_name": match["home"]["longName"],
+						"home_score": home_score,
 						"away_id": match["away"]["id"],
-						"away_name": match["away"]["longName"]
+						"away_name": match["away"]["longName"],
+						"away_score": away_score
 					}
 				)
 
